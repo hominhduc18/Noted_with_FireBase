6f2ce55caee832fc086744f763d666e1a6bca4ec
hominhduc18 <hoduc092@gmail.com>
2023-02-12 12:29:59 +0700

    main

---
+++ app/src/main/java/com/example/noted/Models/Note.java
@@ -0,0 +1,37 @@
+package com.example.noted.Models;
+
+import com.google.firebase.Timestamp;
+
+public class Note {
+
+    public String title;
+    public String content;
+    public Timestamp timestamp;
+
+    public Note() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
